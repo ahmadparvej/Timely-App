e45a8d0e100121bd00bd26c77ebdf097d5494139
Kirandas96 <parvathikiran10@gmail.com>
2022-07-21 02:56:25 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 
 import './App.css';
 import Sidebar from "./Components/Sidebar.tsx";
-import { Box } from "@chakra-ui/react";
-
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import Navbar from "./Components/Navbar.tsx";
 import Freetrial from './Components/signup/Freetrial';
+import { Route, Routes } from 'react-router-dom';
+// import { WithSubnavigation } from './Components/Navbar';
+import { Login } from './Components/Login';
 
 
 function App() {
 
 
   return (
+    <ChakraProvider>
     <Box className="App">
-      <Sidebar/>
-       <Freetrial/>
+      <Box >
+         <Navbar/>
+      </Box>
+      
+      <Routes>
+        <Route path={"/login"} element={<Login/>}/>
+      </Routes>
+      {/* <Sidebar/> */}
+       {/* <Freetrial/> */}
     </Box>
+    </ChakraProvider>
   );
 }
 
---
+++ src/Routes/MainpageRoutes.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MainpageRoutes = () => {
+  return (
+    <div>MainpageRoutes</div>
+  )
+}
