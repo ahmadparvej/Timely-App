479bd97cf110eb03543547cb07d6b2083913d61e
ahmadparvej <ahmadparvej58@gmail.com>
2022-07-20 20:35:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@
 import './App.css';
 import Sidebar from "./Components/Sidebar.tsx";
 import { Box } from "@chakra-ui/react";
-
 import Freetrial from './Components/signup/Freetrial';
 
-
 function App() {
-
-
   return (
     <Box className="App">
       <Sidebar/>
