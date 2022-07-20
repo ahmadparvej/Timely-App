b80d0b109a8bf994b6b1cf06715ae88506472bec
AFFAN-AHMAD <96197969+AFFAN-AHMAD@users.noreply.github.com>
2022-07-21 01:19:40 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
+import Project from './Components/projectsNav/Project';
+
 function App() {
   return (
     <div className="App">
       <h1>Timely.com</h1>
+      <Project/>
     </div>
   );
 }
---
+++ src/Components/projectsNav/project.styles.jsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+display:flex;
+width:100%;
+height: 120px;
+font-family:Circular,-apple-system,BlinkMacSystemFont,system,sans-serif;
+justify-content: space-around;
+position: sticky;
+top:0;
+`
+
+export const LeftDiv = styled.div`
+width:30%;
+
+`
+
+export const RightDiv = styled.div`
+width:60%;
+
+`
+
+export const ProjectH1 = styled.h1`
+`
