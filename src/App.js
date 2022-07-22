3e70a7d37f515bab845931d33bd3137a2472f79e
ahmadparvej <ahmadparvej58@gmail.com>
2022-07-22 10:28:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import "./App.css";
 import Sidebar from "./Components/Sidebar.tsx";
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import Freetrial from "./Components/signup/Freetrial";
 import { MainpageRoutes } from "./Routes/MainpageRoutes";
 import Navbar from "./Components/Navbar/Navbar.tsx";
-// import HomePart1 from "./Components/HomePage/HomePart1";
-
-
+import HomePart1 from "./Components/HomePage/HomePart1";
 
 function App() {
   return (
-    <ChakraProvider>
       <Box className="App">
-      
       <Box>
           <Navbar />
           <HomePart1/>
       </Box>
-
-        {/* <MainpageRoutes/> */}
         <Sidebar/>
         <Freetrial/>
       </Box>
-    </ChakraProvider>
   );
 }
 
